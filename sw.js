eb1495354f776ed9fa8c901c19b949e202d5fee3
Babsinogen-alt <babarindekunle7@gmail.com>
2026-09-07 23:21:13 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="babsinogen-v2.5.3";
+const CACHE="babsinogen-v2.5.5";
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.add("./index.html")).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
